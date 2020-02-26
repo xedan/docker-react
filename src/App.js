0e67ba445fdc2133ba5cc9fc97d7a126f9001ec8
Dan <dan@Dans-MacBook-Pro.local>
2020-02-26 09:00:15 +0100

    v3 test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! updated ;-) 2
+          Hi there! updated ;-) v3
         </p>
         <a
           className="App-link"
